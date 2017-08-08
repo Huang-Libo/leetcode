9aee704e31bfbec2bc7d6786aab41ea29a7c93df
Huang-Libo <LiboHwang+IT@gmail.com>
2017-08-08 13:52:39 +0800

    Add 20-ValidParentheses

--- 12-IntegerToRoman/12-IntegerToRoman/main.c
+++ 12-IntegerToRoman/12-IntegerToRoman/main.c
@@ -165,6 +165,19 @@ char* romanFromDigit(int digit, int index) {
     return currentRoman;
 }
 
+/* 这个写法代码很短, 不过时间和空间花费较多
+char* intToRoman(int num) {
+    const char *M[] = {"", "M", "MM", "MMM"};
+    const char *C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    const char *X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    const char *I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    
+    char *result = (char *) malloc(sizeof(char) * 32);
+    sprintf(result, "%s%s%s%s", M[num/1000], C[num%1000/100], X[num%100/10], I[num%10]);
+    return result;
+}
+ */
+
 /* 时间和空间都比较省, 并且思路较清晰的一种解法:
 char* intToRoman(int num) {
     char *ans;
