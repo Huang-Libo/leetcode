a49624cac9d831586d879bb9b50de3b13e05b3a8
Huang-Libo <LiboHwang+IT@gmail.com>
2017-08-09 23:40:58 +0800

    Add 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists/main.c
@@ -0,0 +1,43 @@
+//
+//  main.c
+//  21-MergeTwoSortedLists
+//
+//  Created by HuangLibo on 2017/8/9.
+//  Copyright © 2017年 Huang Libo. All rights reserved.
+//
+
+/*
+ 21. Merge Two Sorted Lists
+ 
+ Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 1000
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+int main(int argc, const char * argv[]) {
+    
+    struct ListNode * listNode1 = (struct ListNode *)malloc(sizeof(struct ListNode) * 1000);
+    
+    return 0;
+}
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    
+}
