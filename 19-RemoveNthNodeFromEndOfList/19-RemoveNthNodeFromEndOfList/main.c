9b9913648466773275ed25c95168157010779acd
Huang-Libo <LiboHwang+IT@gmail.com>
2017-08-19 20:18:22 +0800

    Add 19's thought

---
+++ 19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList/main.c
@@ -0,0 +1,43 @@
+//
+//  main.c
+//  19-RemoveNthNodeFromEndOfList
+//
+//  Created by HuangLibo on 2017/8/19.
+//  Copyright © 2017年 Huang Libo. All rights reserved.
+//
+
+/*
+ 19. Remove Nth Node From End of List
+
+ Given a linked list, remove the nth node from the end of list and return its head.
+ 
+ For example,
+ 
+ Given linked list: 1->2->3->4->5, and n = 2.
+ 
+ After removing the second node from the end, the linked list becomes 1->2->3->5.
+ Note:
+ Given n will always be valid.
+ Try to do this in one pass.
+ */
+
+#include <stdio.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode * removeNthFromEnd(struct ListNode * head, int n) {
+    
+    // 思路: 使用一个相距 n 的指针, 获取到第 n 个节点的前一个节点.
+}
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
